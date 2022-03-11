553b94cf97f2bee89463d7986804165fcd34b65d
Kyle Tu <kyle.tu@sjsu.edu>
2022-03-10 20:24:33 -0800

    reduced particle count for main page landing background effect

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -40,8 +40,12 @@ function Landing() {
 
     return(
         <div id="landing" className="MainContent">
-            
-            <ParticlesBg type="cobweb" color="#0055A2" bg={{position: "absolute", zIndex: -1, top: 0, left: 0, backgroundColor: "#282c34"}} style={{height:'100vh'}} num={420}/>
+            {(windowDimensions.width > 900) ? (
+                 <ParticlesBg type="cobweb" color="#0055A2" bg={{position: "absolute", zIndex: -1, top: 0, left: 0, backgroundColor: "#282c34"}} style={{height:'100vh'}} num={420}/>
+            ) : (
+                <ParticlesBg type="cobweb" color="#0055A2" bg={{position: "absolute", zIndex: -1, top: 0, left: 0, backgroundColor: "#282c34"}} style={{height:'100vh'}} num={100}/>
+            )}
+
             <Fade>
                 <div className="landing" style={{height:'auto'}}>
                     <p className="Title" style={{height:'auto'}}>Hi! My name is Kyle Tu</p>
